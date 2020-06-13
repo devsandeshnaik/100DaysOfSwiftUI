3f2fdd70fd52e6ccbd86e64a90dd9b1abcd37bfe
Sandesh Naik <devsandeshnaik@gmail.com>
2020-06-13 21:31:07 +0530

    Completed day 45 of 100 Completed Day 35 challenge

---
+++ Drawing/Drawing/ShapeAnimation.swift
@@ -0,0 +1,21 @@
+//
+//  ShapeAnimation.swift
+//  Drawing
+//
+//  Created by Sandesh on 03/06/20.
+//  Copyright © 2020 sandesh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ShapeAnimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShapeAnimation_Previews: PreviewProvider {
+    static var previews: some View {
+        ShapeAnimation()
+    }
+}
