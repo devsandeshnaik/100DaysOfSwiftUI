545ad5359af0013de44230f1707c29224494b62c
devsandeshnaik <devsandeshnaik@gmail.com>
2020-11-20 22:47:08 +0530

    Completed Instafilter

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -9,29 +9,118 @@ import CoreImage
 import CoreImage.CIFilterBuiltins
 import SwiftUI
 
+
+
 struct ContentView: View {
     
     @State private var image: Image?
-    @State private var isShowingImagePicker: Bool = false
-    @State private var inputUIImage: UIImage?
+    @State private var filterIntensity = 0.5
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+    @State private var showingActionSheet = false
+    @State private var processedImage: UIImage?
+    
+    @State private var filter: CIFilter = CIFilter.sepiaTone()
+    let context = CIContext()
+    
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-            
-            Button("Select Image") {
-                self.isShowingImagePicker = true
+        let intsensity = Binding<Double>(
+            get: { self.filterIntensity },
+            set: {
+                self.filterIntensity = $0
+                self.applyProcessing()
             }
-        }
-        .sheet(isPresented: $isShowingImagePicker, onDismiss: loadImage) {
-            ImagePicker(image: $inputUIImage)
+        )
+        
+        return NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(Color.secondary)
+                    if image != nil {
+                        image?
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        Text("Tap to select the image")
+                            .foregroundColor(.white)
+                    }
+                }
+                .onTapGesture {
+                    self.showingImagePicker = true
+                }
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: intsensity)
+                }.padding()
+                
+                HStack {
+                    Button("Change Filter") {
+                        self.showingActionSheet = true
+                    }
+                    
+                    Spacer()
+                    
+                    Button("Save") {
+                        guard let processedImage = self.processedImage else { return }
+                        let imageSaver = ImageSaver()
+                        imageSaver.successHandler = { print("Success")}
+                        imageSaver.errorHandler = { print("Error: \($0.localizedDescription)") }
+                        imageSaver.writeToPhotoAlbum(image: processedImage)
+                    }
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationBarTitle("Instafilter")
+            .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+                ImagePicker(image: self.$inputImage)
+            }
+            .actionSheet(isPresented: $showingActionSheet) {
+                ActionSheet(title: Text("Select a Filter"), buttons: [
+                    .default(Text("Crystallize")) { self.setFilter(CIFilter.crystallize())},
+                    .default(Text("Edges")) { self.setFilter(CIFilter.edges())},
+                    .default(Text("Gaussian Blur")) { self.setFilter(CIFilter.gaussianBlur())},
+                    .default(Text("Pixellate")){ self.setFilter(CIFilter.pixellate())},
+                    .default(Text("Sepia Tone")) { self.setFilter(CIFilter.sepiaTone())},
+                    .default(Text("Unsharp Mask")) { self.setFilter(CIFilter.unsharpMask())},
+                    .default(Text("Vignette")) { self.setFilter(CIFilter.vignette())},
+                    .cancel()
+                ])
+            }
         }
     }
     
     func loadImage() {
-        guard let inputImage = inputUIImage else { return }
-        image = Image(uiImage: inputImage)
+        guard  let inputImage = inputImage else { return }
+        let beginImage = CIImage(image: inputImage)
+        filter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+    
+    func applyProcessing() {
+        let inputKeys = filter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) {
+            filter.setValue(filterIntensity, forKey: kCIInputIntensityKey)
+        }
+        if inputKeys.contains(kCIInputRadiusKey) {
+            filter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey)
+        }
+        if inputKeys.contains(kCIInputScaleKey) {
+            filter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey)
+        }
+        guard let outputImage = filter.outputImage else { return }
+        if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgImage)
+            processedImage = uiImage
+            image = Image(uiImage: uiImage)
+        }
+    }
+    
+    func setFilter(_ filter: CIFilter) {
+        self.filter = filter
+        loadImage()
     }
 }
 
--- Instafilter/Instafilter/ImagePicker.swift
+++ Instafilter/Instafilter/ImagePicker.swift
@@ -2,45 +2,42 @@
 //  ImagePicker.swift
 //  Instafilter
 //
-//  Created by Sandesh on 24/09/20.
+//  Created by Sandesh on 20/11/20.
 //
 
 import SwiftUI
 
+
 struct ImagePicker: UIViewControllerRepresentable {
-    
-    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-        
-        var parent: ImagePicker
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
         
         init(_ parent: ImagePicker) {
             self.parent = parent
         }
         
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            if let image = info[.originalImage] as? UIImage {
-                parent.image = image
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
             }
             parent.presentationMode.wrappedValue.dismiss()
         }
-    
     }
     
-    @Binding var image: UIImage?
     @Environment(\.presentationMode) var presentationMode
+    @Binding var image: UIImage?
     
-    func makeCoordinator() -> Coordinator {
-        return Coordinator(self)
-    }
-    
-    func makeUIViewController(context: Context) -> UIImagePickerController {
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
         let picker = UIImagePickerController()
         picker.delegate = context.coordinator
         return picker
     }
     
-    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: UIViewControllerRepresentableContext<ImagePicker>) {
         
     }
-     
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
 }
