21a045e3f1f97275029bab9dc94d1d9e1badd43c
Sandesh Naik <devsandeshnaik@gmail.com>
2022-09-11 16:00:38 +0530

    Completed Day 70

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Sandesh Naik on 10/09/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
